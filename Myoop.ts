0bd7a2abdbcbbb78ed6b298cd825d9fb594a1bc2
Hassan_Ali <165322829+Hassanfarid6@users.noreply.github.com>
2024-06-10 00:23:20 +0500

    Add files via upload

---
+++ Myoop.ts
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+class Student {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Person {
+  students: Student[] = [];
+  addStudent(obj: Student) {
+    this.students.push(obj);
+  }
+}
+let person = new Person();
+
+let programStart = async (person: Person) => {
+  console.log(chalk.bold.red("<<<   WelCome in My Oop's Project    >>>"));
+
+  do {
+    const { select } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "select",
+        message: "Select your choice",
+        choices: ["Add Student", "Show Student", "Exit"],
+      },
+    ]);
+    if (select === "Add Student") {
+      const { name } = await inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Enter your name",
+        },
+      ]);
+      person.addStudent(new Student(name));
+    } else if (select === "Show Student") {
+      console.log(person.students);
+    } else {
+      break;
+    }
+  } while (true);
+};
+programStart(person);
